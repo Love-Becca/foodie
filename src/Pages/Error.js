a9ff48ca43f3472a90a33719de81823a4bb361f0
Love-Becca <follycube2020@gmail.com>
2023-06-28 00:18:31 +0100

    Fix warning

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -3,6 +3,7 @@ import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
     const error = useRouteError()
+    console.log(error);
     return(
         <div>
             <h1>Uh oh! We've got a problem</h1>
